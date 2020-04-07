9f241c68099330fe188b39ad8080f2a44b5583bc
Gabriel Perry <gperry22@gmail.com>
2020-04-07 18:53:29 -0400

    new build

---
+++ client/build/precache-manifest.96050d1d45a59a94f41fe4f973dd4004.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "05b2752fdda55f9fb04c",
+    "url": "/static/js/main.b9905b6c.chunk.js"
+  },
+  {
+    "revision": "10d4fc86be435d7dc6d3",
+    "url": "/static/js/2.0a40ed4b.chunk.js"
+  },
+  {
+    "revision": "05b2752fdda55f9fb04c",
+    "url": "/static/css/main.5498db54.chunk.css"
+  },
+  {
+    "revision": "c2929420964492d92ff65d1fcdaa8255",
+    "url": "/index.html"
+  }
+];
