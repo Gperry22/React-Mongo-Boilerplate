cd206811b985c24df733a25ca10e8d05ae38aaff
Gabriel Perry <gperry22@gmail.com>
2020-04-07 20:45:43 -0400

    build

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,7 +1,16 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
   app.use(
-    '/api',
+    '/api/fruit',
+    createProxyMiddleware({
+      target: 'https://localhost:3001',
+        changeOrigin: true,
+        secure: false
+    })
+    );
+
+     app.use(
+    '/api/sweet',
     createProxyMiddleware({
       target: 'https://localhost:3001',
         changeOrigin: true,
