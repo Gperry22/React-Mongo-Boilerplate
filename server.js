18827868591f018331b29f423feafb73e55a28c6
Gabriel Perry <gperry22@gmail.com>
2020-04-07 20:27:51 -0400

    new build

--- server.js
+++ server.js
@@ -9,21 +9,21 @@ const PORT = process.env.PORT || 3001;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var databaseToUse = ""
+// var databaseToUse = ""
 
-if (process.env.NODE_ENV === "production") {
+// if (process.env.NODE_ENV === "production") {
 	app.use(express.static('client/build'));
-	databaseToUse = "mongodb://gabe:a123456@ds133601.mlab.com:33601/heroku_nf0ztkqg";
-}
-else {
-	databaseToUse = 'mongodb://localhost/reactBoilerplate';
-	console.log("DEVELOPMENT")
-}
+// 	databaseToUse = "mongodb://gabe:a123456@ds133601.mlab.com:33601/heroku_nf0ztkqg";
+// }
+// else {
+// 	databaseToUse = 'mongodb://localhost/reactBoilerplate';
+// 	console.log("DEVELOPMENT")
+// }
 
 
 app.use(routes);
 
-const MONGODB_URI = process.env.MONGODB_URI || databaseToUse
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://gabe:a123456@ds133601.mlab.com:33601/heroku_nf0ztkqg"
 
 
 mongoose.Promise = global.Promise;
